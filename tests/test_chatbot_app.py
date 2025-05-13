3ca68811eccfe905759c1c6528fe51a06fc2df43
munsif135 <mohammed.munsif@rootcodelabs.com>
2025-05-13 17:32:54 +0530

    test cicd with error

--- tests/test_chatbot_app.py
+++ tests/test_chatbot_app.py
@@ -45,3 +45,7 @@ def test_mul_endpoint():
     response = client.post("/mul", params={"a": -1, "b": -1})
     assert response.status_code == 200
     assert response.json() == {"result": 1}
+
+    response = client.post("/mul", params={"a": 2, "b": -1})
+    assert response.status_code == 200
+    assert response.json() == {"result": 2}
